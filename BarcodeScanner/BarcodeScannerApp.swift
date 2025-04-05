053f3db896f3615870e9e612a834670a373f9985
Suman <sumanksk2006@gmail.com>
2025-04-05 18:05:11 -0500

    Initial setup for repo

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Suman Kumar on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerTests/BarcodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerTests.swift
+//  BarcodeScannerTests
+//
+//  Created by Suman Kumar on 4/5/25.
+//
+
+import Testing
+@testable import BarcodeScanner
+
+struct BarcodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
